6ea2180742658280a3a14023539abd1577789e76
Guillermo Gonzalez <guillermo.gonzalez@me.com>
2015-03-26 09:06:55 +0100

    Add type-safe NSValueTransformer helper methods

---
+++ Groot/Groot.h
@@ -0,0 +1,19 @@
+//
+//  Groot.h
+//  Groot
+//
+//  Created by Guille Gonzalez on 25/03/15.
+//  Copyright (c) 2015 Guille González. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Groot.
+FOUNDATION_EXPORT double GrootVersionNumber;
+
+//! Project version string for Groot.
+FOUNDATION_EXPORT const unsigned char GrootVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Groot/PublicHeader.h>
+
+
---
+++ Groot/ValueTransformer.swift
@@ -0,0 +1,88 @@
+//
+//  ValueTransformer.swift
+//  Groot
+//
+//  Created by Guille Gonzalez on 25/03/15.
+//  Copyright (c) 2015 Guille González. All rights reserved.
+//
+
+import Foundation
+
+private class ValueTransformer: NSValueTransformer {
+    let transform: (AnyObject?) -> (AnyObject?)
+    
+    init(transform: (AnyObject?) -> (AnyObject?)) {
+        self.transform = transform
+    }
+    
+    // MARK: NSValueTransformer
+    
+    override class func allowsReverseTransformation() -> Bool {
+        return false;
+    }
+    
+    override class func transformedValueClass() -> AnyClass {
+        return NSObject.self
+    }
+    
+    override func transformedValue(value: AnyObject?) -> AnyObject? {
+        return transform(value)
+    }
+}
+
+private class ReversibleValueTransformer: ValueTransformer {
+    let reverseTransform: (AnyObject?) -> (AnyObject?)
+    
+    init(transform: (AnyObject?) -> (AnyObject?), reverseTransform: (AnyObject?) -> (AnyObject?)) {
+        self.reverseTransform = reverseTransform
+        super.init(transform: transform)
+    }
+    
+    // MARK: NSValueTransformer
+    
+    override class func allowsReverseTransformation() -> Bool {
+        return true;
+    }
+    
+    override func reverseTransformedValue(value: AnyObject?) -> AnyObject? {
+        return reverseTransform(value)
+    }
+}
+
+public extension NSValueTransformer {
+    class func setValueTransformerWithName<T, U>(name: String, transform: (T) -> (U?)) {
+        let transformer = ValueTransformer { value in
+            if let v = value as? T {
+                if let r: AnyObject? = transform(v) as? AnyObject? {
+                    return r
+                }
+            }
+            
+            return nil
+        }
+        
+        self.setValueTransformer(transformer, forName: name)
+    }
+    
+    class func setValueTransformerWithName<T, U>(name: String, transform: (T) -> (U?), reverseTransform: (U) -> (T?)) {
+        let transformer = ReversibleValueTransformer(transform: { value in
+            if let v = value as? T {
+                if let r: AnyObject? = transform(v) as? AnyObject? {
+                    return r
+                }
+            }
+            
+            return nil
+        }, reverseTransform: { value in
+            if let v = value as? U {
+                if let r: AnyObject? = reverseTransform(v) as? AnyObject? {
+                    return r
+                }
+            }
+            
+            return nil
+        })
+        
+        self.setValueTransformer(transformer, forName: name)
+    }
+}
