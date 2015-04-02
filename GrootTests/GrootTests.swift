6cfb4a12cf49d1bacaf2859d61dfb9fd49cfed8b
Guillermo Gonzalez <guillermo.gonzalez@me.com>
2015-04-02 16:33:25 +0200

    Add merge test

--- GrootTests/GrootTests.swift
+++ GrootTests/GrootTests.swift
@@ -140,7 +140,103 @@ class GrootTests: XCTestCase {
         XCTAssertEqual(Groot.errorDomain, error!.domain, "should return a Groot error")
         XCTAssertEqual(Groot.Error.InvalidJSONObject.rawValue, error!.code, "should return an invalid JSON error")
     }
+    
+    func testMergeObject() {
+        let batmanJSON: JSONObject = [
+            "name": "Batman",
+            "real_name": "Bruce Wayne",
+            "id": "1699",
+            "powers": [
+                [
+                    "id": "4",
+                    "name": "Agility"
+                ],
+                NSNull(),
+                [
+                    "id": "9",
+                    "name": "Insanely Rich"
+                ]
+            ],
+            "publisher": [
+                "id": "10",
+                "name": "DC Comics"
+            ]
+        ]
+        
+        var error: NSError? = nil
+        let batman = Character.fromJSONObject(batmanJSON, inContext: context!, mergeChanges: true, error: &error)
+        
+        XCTAssertNil(error)
+        XCTAssertNotNil(batman)
+        
+        let updateJSON: JSONArray = [
+            [
+                "id": "1699",
+                "real_name": NSNull(),  // Should reset Batman real name
+                "publisher" : [
+                    "id": "10",
+                    "name": "DC Comics"   // Should update the publisher name
+                ]
+            ],
+            [
+                "id": "1455",
+                "name": "Iron Man",
+                "real_name": "Tony Stark",
+                "powers": [
+                    [
+                        "id": "31",
+                        "name": "Power Suit"
+                    ],
+                    [
+                        "id": "9",
+                        "name": "Filthy Rich" // Should update the 'Rich' power name
+                    ]
+                ]
+            ]
+        ]
+        
+        let characters: [Character]? = importJSONArray(updateJSON, inContext: context!, mergeChanges: true, error: &error)
+        
+        XCTAssertNil(error)
+        XCTAssertNotNil(characters)
+        
+        XCTAssertEqual(2, characters!.count)
+        
+        let updatedBatman = characters![0]
+        
+        XCTAssertEqual("Batman", updatedBatman.name!)
+        XCTAssertEqual(1699, updatedBatman.identifier)
+        XCTAssertNil(updatedBatman.realName, "should update explicit null values")
+        
+        let agility = updatedBatman.powers![0] as! Power
+        
+        XCTAssertEqual(4, agility.identifier)
+        XCTAssertEqual("Agility", agility.name!)
+        
+        let batmanRich = updatedBatman.powers![1] as! Power
+        
+        XCTAssertEqual(9, batmanRich.identifier, "should serialize to-many relationships")
+        XCTAssertEqual("Filthy Rich", batmanRich.name!, "should serialize to-many relationships")
+        
+        XCTAssertEqual(10, updatedBatman.publisher!.identifier);
+        XCTAssertEqual("DC Comics", updatedBatman.publisher!.name!)
+        
+        let ironMan = characters![1]
+        
+        XCTAssertEqual("Iron Man", ironMan.name!)
+        XCTAssertEqual(1455, ironMan.identifier)
+        XCTAssertEqual("Tony Stark", ironMan.realName!, "should serialize attributes")
+        
+        let powerSuit = ironMan.powers![0] as! Power
+        
+        XCTAssertEqual(31, powerSuit.identifier)
+        XCTAssertEqual("Power Suit", powerSuit.name!)
+        
+        let ironManRich = ironMan.powers![1] as! Power
+        
+        XCTAssertEqual(9, ironManRich.identifier)
+        XCTAssertEqual("Filthy Rich", ironManRich.name!)
+        
+        XCTAssert(batmanRich === ironManRich, "should properly merge relationships")
+    }
 }
-
-/*
-*/
