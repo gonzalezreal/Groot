416b33de2866181dea5096a4978a47a11540b670
Guillermo Gonzalez <gonzalezreal@icloud.com>
2015-08-19 16:31:16 +0200

    Implement composite uniquing strategy

--- Groot/Private/GRTCompositeUniquingSerializationStrategy.m
+++ Groot/Private/GRTCompositeUniquingSerializationStrategy.m
@@ -21,6 +21,11 @@
 // THE SOFTWARE.
 
 #import "GRTCompositeUniquingSerializationStrategy.h"
+#import "GRTError.h"
+
+#import "NSEntityDescription+Groot.h"
+#import "NSAttributeDescription+Groot.h"
+#import "NSManagedObject+Groot.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
@@ -47,7 +52,113 @@ NS_ASSUME_NONNULL_BEGIN
                       inContext:(NSManagedObjectContext *)context
                           error:(NSError *__autoreleasing  __nullable * __nullable)outError
 {
-    return nil;
+    NSMutableArray * __block managedObjects = [NSMutableArray array];
+    NSError * __block error = nil;
+    
+    [context performBlockAndWait:^{
+        for (id obj in array) {
+            if (obj == [NSNull null]) {
+                continue;
+            }
+            
+            if (![obj isKindOfClass:[NSDictionary class]]) {
+                // This strategy can only serialize JSON dictionaries
+                NSString *format = NSLocalizedString(@"Cannot serialize '%@' into entity '%@'.", @"Groot");
+                NSString *message = [NSString stringWithFormat:format, obj, self.entity.name];
+                error = [NSError errorWithDomain:GRTErrorDomain
+                                            code:GRTErrorInvalidJSONObject
+                                        userInfo:@{ NSLocalizedDescriptionKey: message }];
+                
+                break;
+            }
+            
+            NSManagedObject *managedObject = [self serializeJSONDictionary:obj
+                                                                 inContext:context
+                                                                     error:&error];
+            
+            if (error != nil) {
+                break;
+            }
+            
+            [managedObjects addObject:managedObject];
+        }
+    }];
+    
+    if (error != nil) {
+        if (outError != nil) {
+            *outError = error;
+        }
+    }
+    
+    return managedObjects;
+}
+
+#pragma mark - Private
+
+- (NSManagedObject *)serializeJSONDictionary:(NSDictionary *)dictionary
+                                   inContext:(NSManagedObjectContext *)context
+                                       error:(NSError *__autoreleasing  __nullable * __nullable)outError
+{
+    NSError *error = nil;
+    NSManagedObject *managedObject = [self existingObjectWithJSONDictionary:dictionary inContext:context error:&error];
+    
+    if (error != nil) {
+        if (outError != nil) {
+            *outError = error;
+        }
+        return nil;
+    }
+    
+    if (managedObject == nil) {
+        NSString *entityName = [self.entity grt_subentityNameForJSONValue:dictionary];
+        managedObject = [NSEntityDescription insertNewObjectForEntityForName:entityName
+                                                      inManagedObjectContext:context];
+    }
+    
+    [managedObject grt_serializeJSONDictionary:dictionary mergeChanges:YES error:&error];
+    
+    if (error != nil) {
+        [context deleteObject:managedObject];
+        
+        if (outError != nil) {
+            *outError = error;
+        }
+        
+        return nil;
+    }
+    
+    return managedObject;
+}
+
+- (nullable NSManagedObject *)existingObjectWithJSONDictionary:(NSDictionary *)dictionary
+                                                     inContext:(NSManagedObjectContext *)context
+                                                         error:(NSError *__autoreleasing  __nullable * __nullable)outError
+{
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    fetchRequest.entity = self.entity;
+    fetchRequest.predicate = [self predicateForJSONDictionary:dictionary];
+    
+    return [context executeFetchRequest:fetchRequest error:outError].firstObject;
+}
+
+- (NSPredicate *)predicateForJSONDictionary:(NSDictionary *)dictionary {
+    NSMutableArray *subpredicates = [NSMutableArray arrayWithCapacity:self.uniqueAttributes.count];
+    
+    for (NSAttributeDescription *attribute in self.uniqueAttributes) {
+        id value = [attribute grt_valueForJSONValue:dictionary];
+        
+        NSExpression *leftExpression = [NSExpression expressionForKeyPath:attribute.name];
+        NSExpression *rightExpression = [NSExpression expressionForConstantValue:value];
+        
+        NSComparisonPredicate *subpredicate = [NSComparisonPredicate predicateWithLeftExpression:leftExpression
+                                                                                 rightExpression:rightExpression
+                                                                                        modifier:NSDirectPredicateModifier
+                                                                                            type:NSEqualToPredicateOperatorType
+                                                                                         options:0];
+        [subpredicates addObject:subpredicate];
+    }
+    
+    return [NSCompoundPredicate andPredicateWithSubpredicates:subpredicates];
 }
 
 @end
